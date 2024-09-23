ab62710b8cf8ebe24e02243e264486ae6bd9c383
AngelMR <mra7588@gmail.com>
2024-09-22 18:30:53 -0600

    feat: added section banner and services

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      maxWidth: {
+        'screen-container': '1075px'
+      },
+      colors: {
+        'blue-light': '#C2C2FF',
+        'morado-light': '#8892B3',
+        'green-light': '#F0F4FF',
+        'green-light-2': '#E1E8FF',
+        'dark-text': '#3F3F3F',
+        'dark-bold': '#000A0A'
+      }
+    },
+  },
+  plugins: [],
+}
+
